347fc21f10fc41832ef81241fd6c183ffb1eada6
X-Coder <gmod@x-coder.de>
2016-01-28 04:08:36 +0100

    Added auto resources files downloading

---
+++ lua/autorun/server/resources.lua
@@ -0,0 +1,4 @@
+resource.AddFile( "materials/xcsca/control.vmt" )
+resource.AddFile( "materials/xcsca/screen.vmt" )
+resource.AddFile( "materials/xcsca/utd.vmt" )
+resource.AddFile( "models/weapons/xcsca/c_utd.mdl" )
