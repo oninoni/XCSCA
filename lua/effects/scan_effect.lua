42c50b237919345d29c6845cd95c71f93f407bd9
Oninoni <jan@cheap-purple.com>
2016-01-28 03:59:34 +0100

    no message

--- lua/effects/scan_effect.lua
+++ lua/effects/scan_effect.lua
@@ -1,22 +1,23 @@
 function EFFECT:Init(data)
-	print("Blubb")
-	
 	local e = data:GetEntity()
-	print(e)
 	if(not IsValid(e))then return end
 	self.Entity:SetModel(e:GetModel())
 	self.Entity:SetPos(e:GetPos())
 	self.Entity:SetAngles(e:GetAngles())
 	self.Entity:SetMaterial("models/wireframe")
+	
+	self.Entity:SetParent(e)
 end
 
+local scanPosition = 0
+
 function EFFECT:Think()
+	scanPosition++
 	return true
 end
 
 function EFFECT:Render()
-
-
-
-	self.Entity:DrawModel()
+	self.Entity:SetRenderClipPlaneEnabled( true )
+	self.Entity:SetRenderClipPlane( self.Entity:GetUp(), self.Entity:GetPos() + Vector(0,0,scanPosition))
+	self.Entity:DrawModel();
 end
