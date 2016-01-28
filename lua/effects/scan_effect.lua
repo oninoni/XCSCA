72086483a325de3a9fc1a13bd48240a0f3296a54
Oninoni <jan@cheap-purple.com>
2016-01-28 17:40:48 +0100

    Scan Effect Works

--- lua/effects/scan_effect.lua
+++ lua/effects/scan_effect.lua
@@ -7,19 +7,47 @@ function EFFECT:Init(data)
 	self.Entity:SetMaterial("models/wireframe")
 	
 	self.Entity:SetParent(e)
+	
+	self.min, self.max = e:GetModelBounds()
+	self.height = self.max.z - self.min.z
+	
+	
+	self.offset = self.height / 20
+	
+	self.height = self.height + self.offset * 2
+	
+	self.scanPosition = 0
+	renderClip = true
 end
 
-local scanPosition = 0
-
 function EFFECT:Think()
-	scanPosition = scanPosition + 1 
+	self.scanPosition = self.scanPosition + self.height/500
+	if(self.scanPosition > self.height) then self.scanPosition = 0 end
+	
+	//Removes the Effect when Target gets Deleted
+	
+	if(self.Entity:GetParent() == NULL)then
+		print("Entity deleted")
+		return false
+	end
+	
 	return true
 end
 
 function EFFECT:Render()
-	local normal = self.Entity:GetUp()
-	local position = normal:Dot( self.Entity:GetPos() + Vector(0,0,scanPosition))
-	self.Entity:SetRenderClipPlaneEnabled( true )
-	self.Entity:SetRenderClipPlane( normal, position)
-	self.Entity:DrawModel();
+	local normal = self.Entity:GetParent():GetUp()
+	local position = normal:Dot(self.Entity:GetParent():GetPos())
+	cam.Start3D(EyePos() + normal*0.01,EyeAngles())
+		render.EnableClipping(true)
+		render.PushCustomClipPlane( normal, position - self.height/2 + (self.scanPosition - self.offset))
+		render.PushCustomClipPlane(-normal,-position + self.height/2 - (self.scanPosition + self.offset))
+		self.Entity:DrawModel();
+		render.PopCustomClipPlane()
+		render.PopCustomClipPlane()
+		render.EnableClipping(false)
+	cam.End3D()
+	//self.Entity:SetRenderClipPlaneEnabled( true )
+	//self.Entity:SetRenderClipPlane( normal, position - height/2 + scanPosition - height/10)
+	//self.Entity:SetRenderClipPlane( normal, position - height/2 + scanPosition - height/10)
+	
 end
